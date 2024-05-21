6b897e978606e00ce95a8d40534a0bfc40cceaa1
warren aroni <warren.aroni@provistacorp.com>
2024-05-21 00:54:56 -0500

    update

--- CustomSecuritySample/Properties/Settings.Designer.cs
+++ CustomSecuritySample/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace Microsoft.Samples.ReportingServices.CustomSecurity.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "15.7.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.9.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -25,21 +25,21 @@ namespace Microsoft.Samples.ReportingServices.CustomSecurity.Properties {
         
         [global::System.Configuration.ApplicationScopedSettingAttribute()]
         [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.DefaultSettingValueAttribute("Server=localhost; Integrated Security=SSPI;database=UserAccounts")]
+        public string Database_ConnectionString {
+            get {
+                return ((string)(this["Database_ConnectionString"]));
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
         [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.WebServiceUrl)]
         [global::System.Configuration.DefaultSettingValueAttribute("http://localhost:80/ReportServer_SQL2014/ReportService2010.asmx")]
         public string Microsoft_Samples_ReportingServices_CustomSecurity_localhost_ReportingService2010 {
             get {
                 return ((string)(this["Microsoft_Samples_ReportingServices_CustomSecurity_localhost_ReportingService2010" +
                     ""]));
-            }
-        }
-        
-        [global::System.Configuration.ApplicationScopedSettingAttribute()]
-        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
-        [global::System.Configuration.DefaultSettingValueAttribute("Server=localhost; Integrated Security=SSPI;database=UserAccounts")]
-        public string Database_ConnectionString {
-            get {
-                return ((string)(this["Database_ConnectionString"]));
             }
         }
     }
